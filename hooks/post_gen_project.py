2a435d80142cec437344073c7d292d554f5898b9
Erez Rokah <erezrokah@users.noreply.github.com>
2020-11-10 16:16:48 +0200

    Update hooks/post_gen_project.py
    Co-authored-by: ehmicky <ehmicky@users.noreply.github.com>

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,5 +2,5 @@ import subprocess
 
 subprocess.call(['git', 'init'])
 subprocess.call(['npm', 'install'])
-subprocess.call(['git', 'add', '*'])
+subprocess.call(['git', 'add', '.'])
 subprocess.call(['git', 'commit', '-m', 'Initial commit'])
