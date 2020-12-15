85b26f42caf1a2458dac40b1eefbef2038c27f34
Erez Rokah <erezrokah@users.noreply.github.com>
2020-12-15 07:14:34 -0800

    chore: add release-please (#12)

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,14 @@
 import subprocess
+import fileinput
+import os
+
+# This is required since we don't process GitHub actions
+project_slug = '{{ cookiecutter.project_slug }}'
+filename = os.path.join('.github', 'workflows', 'release-please.yml')
+with fileinput.FileInput(filename, inplace=True) as file:
+    for line in file:
+        print(line.replace('project_slug', project_slug), end='')
+
 
 subprocess.call(['git', 'init'])
 subprocess.call(['npm', 'install'])
